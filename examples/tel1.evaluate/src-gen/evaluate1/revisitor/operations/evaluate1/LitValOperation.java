c3d940d996811d2f294b8ffc96cf36a9ae338d73
tdegueul <degueule@cwi.nl>
2018-09-06 20:50:37 +0200

    Fix TEL0/1 buildpaths and re-generate examples

--- examples/tel1.evaluate/src-gen/evaluate1/revisitor/operations/evaluate1/LitValOperation.java
+++ examples/tel1.evaluate/src-gen/evaluate1/revisitor/operations/evaluate1/LitValOperation.java
@@ -1,9 +1,9 @@
 package evaluate1.revisitor.operations.evaluate1;
 
-import evaluate.revisitor.operations.evaluate.ExpOperation;
+import evaluate1.revisitor.operations.evaluate1.ExpOperation;
 import tel1.evaluate.Ctx;
 
 @SuppressWarnings("all")
-public interface LitValOperation extends evaluate.revisitor.operations.evaluate.LitValOperation, ExpOperation, evaluate1.revisitor.operations.evaluate1.ExpOperation {
+public interface LitValOperation extends evaluate.revisitor.operations.evaluate.LitValOperation, ExpOperation, evaluate.revisitor.operations.evaluate.ExpOperation {
   public abstract long eval(final Ctx ctx);
 }
